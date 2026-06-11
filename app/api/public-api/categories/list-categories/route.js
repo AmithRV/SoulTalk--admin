2fd33c9896eda47e3fcf05ef74afc8bfa726750c
Amith <amith@mojgenie.com>
2026-06-12 01:59:39 +0530

    commit

---
+++ app/api/public-api/categories/list-categories/route.js
@@ -0,0 +1,21 @@
+import { NextResponse } from 'next/server';
+import Category from '@/lib/models/Category';
+import { formatZodErrors } from '@/lib/utils';
+import { databaseConnection } from '@/lib/dbConfig';
+
+await databaseConnection();
+
+export async function GET() {
+  try {
+    const categories = await Category.find(query).sort({ createdAt: -1 });
+
+    return NextResponse.json({ data: categories }, { status: 200 });
+  } catch (error) {
+    if (error.name === 'ZodError') {
+      const message = formatZodErrors(error);
+      return NextResponse.json({ message }, { status: 422 });
+    } else {
+      return NextResponse.json({ message: error.message }, { status: 500 });
+    }
+  }
+}
